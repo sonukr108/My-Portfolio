54637b68af178a87f425ec464ff4afee05cb6004
Sonu Kumar <sv808494@gmail.com>
2025-04-14 18:57:47 +0530

    Added contact part

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='px-[10%] py-10 w-full min-h-[140vh] md:min-h-[100vh]'>
+          <div className=' flex flex-col gap-3 items-center justify-center'>
+            <p className='font-bold text-xl lg:text-2xl text-gray-500'>CONTACT</p>
+            <p className='text-xl lg:text-2xl font-semibold pb-3'>Contact <span className='text-blue-400'>With Me</span></p>
+            
+          </div>
+        </div>
+  )
+}
+
+export default Contact
